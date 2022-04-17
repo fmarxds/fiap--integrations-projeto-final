e458de153823b8af41c97a1e169022d04b05a2fc
lugamvazori <lucasoliveiravaz@gmail.com>
2022-04-17 20:42:47 -0300

    Corrigido listener de mensagem.

--- fiap--integrations-consumer/src/main/java/br/com/fiap/integration/fiapintegrationsconsumer/listener/DroneMessageListener.java
+++ fiap--integrations-consumer/src/main/java/br/com/fiap/integration/fiapintegrationsconsumer/listener/DroneMessageListener.java
@@ -3,7 +3,9 @@ package br.com.fiap.integration.fiapintegrationsconsumer.listener;
 import br.com.fiap.integration.fiapintegrationsconsumer.repository.DroneRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -14,9 +16,9 @@ public class DroneMessageListener {
 
     private DroneRepository droneRepository;
 
-    public void receiveMessage(Map<String, String> message) {
-        System.out.println("Received < " + message + " >");
-
-        System.out.println("Message processed...");
+    @RabbitListener(queues = {"queue-drone-sensorial"})
+    public void receiveMessage(@Payload String fileBody) {
+        System.out.println("Message " + fileBody);
     }
+
 }
