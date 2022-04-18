2c7a761d5109656bd1a2256f3da383ee6dd0c5bd
Felipe Marques <fmarques.ds@gmail.com>
2022-04-18 00:32:11 -0300

    fix

--- fiap--integrations-consumer/src/main/java/br/com/fiap/integration/fiapintegrationsconsumer/FiapIntegrationsConsumerApplication.java
+++ fiap--integrations-consumer/src/main/java/br/com/fiap/integration/fiapintegrationsconsumer/FiapIntegrationsConsumerApplication.java
@@ -1,15 +1,30 @@
 package br.com.fiap.integration.fiapintegrationsconsumer;
 
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
 
 @EnableScheduling
 @SpringBootApplication
 public class FiapIntegrationsConsumerApplication {
 
+	@Autowired
+	AmqpAdmin amqpAdmin;
+
 	public static void main(String[] args) {
 		SpringApplication.run(FiapIntegrationsConsumerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void criarFilas() {
+		if (amqpAdmin.getQueueInfo("queue-drone-sensorial") == null) {
+			amqpAdmin.declareQueue(new Queue("queue-drone-sensorial", true));
+		}
 	}
 
 }
---
+++ fiap--integrations-consumer/src/main/java/br/com/fiap/integration/fiapintegrationsconsumer/config/RabbitMQConfig.java
@@ -0,0 +1,19 @@
+package br.com.fiap.integration.fiapintegrationsconsumer.config;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableRabbit
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+}
