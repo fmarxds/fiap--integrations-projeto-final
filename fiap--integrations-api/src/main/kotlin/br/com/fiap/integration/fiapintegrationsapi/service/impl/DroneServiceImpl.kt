df4e6a41e2c063668423d165ff71d12c36d1b951
Felipe Marques <fmarques.ds@gmail.com>
2022-04-17 21:20:10 -0300

    Finalizado fluxo de envio automatico de dados de drone

--- fiap--integrations-api/src/main/kotlin/br/com/fiap/integration/fiapintegrationsapi/service/impl/DroneServiceImpl.kt
+++ fiap--integrations-api/src/main/kotlin/br/com/fiap/integration/fiapintegrationsapi/service/impl/DroneServiceImpl.kt
@@ -3,8 +3,13 @@ package br.com.fiap.integration.fiapintegrationsapi.service.impl
 import br.com.fiap.integration.fiapintegrationsapi.model.DroneData
 import br.com.fiap.integration.fiapintegrationsapi.service.DroneService
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.text.DecimalFormat
+import java.util.*
+import kotlin.random.Random
 
 @Service
 class DroneServiceImpl(
@@ -12,9 +17,28 @@ class DroneServiceImpl(
     private val objectMapper: ObjectMapper,
 ) : DroneService {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun postDroneStatusData(droneData: DroneData) {
         val message = objectMapper.writeValueAsString(droneData)
         rabbitTemplate.convertAndSend("queue-drone-sensorial", message)
+        logger.info("Mensagem enviada: $message")
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    fun postDroneStatusDataBot() {
+
+        val droneData = DroneData(
+            droneId = UUID.randomUUID().toString(),
+            latitude = DecimalFormat("#.######").format(Random.nextDouble(-90.0, 90.0)).toDouble(),
+            longitude = DecimalFormat("#.######").format(Random.nextDouble(-90.0, 90.0)).toDouble(),
+            temperatura = DecimalFormat("#.##").format(Random.nextDouble(-25.0, 40.1)).toDouble(),
+            umidade = DecimalFormat("#.##").format(Random.nextDouble(0.0, 100.1)).toDouble(),
+            ativarRastreamento = listOf(true, false)[Random.nextInt(0, 2)]
+        )
+
+        this.postDroneStatusData(droneData)
+
     }
 
 }
